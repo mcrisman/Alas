463e0f4afd0f4a58c3fdb7185d9cee7f2acc98c4
Crisman Martinez B <34546825+mcrisman@users.noreply.github.com>
2017-12-30 21:29:15 -0500

    Add files via upload
    ConexionDb clase desarrollada en PHP 5, que se conecta a una base de datos.
    Leecliente.php: codigo en php que funciona perfectamente, consulta una tabla en la base de datos mysql y muestra los datos en formato tipo Json.

---
+++ LeeCliente.php
@@ -0,0 +1,16 @@
+﻿<?php
+
+    require_once 'clases/ConexionDb.php';
+    $pdo = new ConexionDb();
+
+
+    $valor='primera';
+    $rows = array();
+    if(isset($valor)) 
+    {
+        $stmt = $pdo->prepare("SELECT * FROM TProyeccion WHERE valor = ?");
+        $stmt->execute(array($valor));
+        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    echo json_encode($rows);
+?>
